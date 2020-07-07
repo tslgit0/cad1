7dbce1ccf9348a310f4260c5076c2ae9c9730bc2
tslgit0 <406668865@qq.com>
2020-07-07 20:22:26 +0800

    resnet18

--- code/train.py
+++ code/train.py
@@ -16,7 +16,7 @@ from sklearn.metrics import confusion_matrix
 from tensorboardX import SummaryWriter
 from torchvision.datasets import ImageFolder
 from sklearn.model_selection import train_test_split
-
+bc=128
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 transform = transforms.Compose([
     transforms.Resize(size=(227, 227)),
@@ -27,16 +27,16 @@ transform = transforms.Compose([
 ])
 
 train_dataset = ImageFolder(root='/home/cad429/code/data/splitk', transform=transform)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=bc, shuffle=True)
 
-test_dataset = ImageFolder(root='../test', transform=transform)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=8, shuffle=True)
+# test_dataset = ImageFolder(root='../test', transform=transform)
+# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=bc, shuffle=True)
 
 model=models.resnet18(pretrained=True)
 for para in model.parameters():
     para.require_grad=False
 model.fc=nn.Linear(512,2)
-optimizer=optim.SGD(model.parameters(),lr=0.001)
+optimizer=optim.SGD(model.parameters(),lr=0.0001)
 loss=nn.CrossEntropyLoss()
 
 
@@ -44,15 +44,19 @@ def train(model,train_loader):
     model.cuda()
     model.train()
     total_loss=0
-    for epoch in range(100):
+    for epoch in range(10):
         print("epoch:", epoch)
         s=0
+        sum=0
         for i,data in enumerate(train_loader):
+
             inputs,labels=data
             inputs=Variable(inputs).cuda()
             labels=Variable(labels).cuda()
+
             optimizer.zero_grad()
             outputs=model(inputs)
+
             loss1=loss(outputs,labels)
             loss1.backward()
             optimizer.step()
@@ -62,17 +66,24 @@ def train(model,train_loader):
             #print(pred)
             pred=pred.cpu().numpy()
             labels=labels.cpu().numpy()
+
             for j in range(len(pred)):
                 #print(len(pred))
                 if pred[j] == labels[j]:
                     s+=1
+            sum=sum+bc
+            if i%bc==0:
+                print('i=',i)
+                loss2=loss1.cpu().detach().numpy()
+                print("loss",loss2)
+                # print("s:",s)
+                # print("train_loader len:",len(train_loader))
+                # sa=len(train_loader)
+                acc=s/sum
+                print("acc: ",acc)
+                s=0
+                sum=0
 
-
-        print("s:",s)
-        print("train_loader len:",len(train_loader))
-        sa=len(train_loader)
-        acc=s/(sa*8)
-        print("acc: ",acc)
 
     # model.eval()
     # s1=0
